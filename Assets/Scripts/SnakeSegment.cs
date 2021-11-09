f042c9436dac02dc89c66abc326e266dcf993970
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-11-09 17:17:29 +0100

    Got started on snake segments

---
+++ Assets/Scripts/SnakeSegment.cs
@@ -0,0 +1,29 @@
+﻿using m1m1c_3DAstarPathfinding;
+using System;
+using System.Collections;
+using UnityEngine;
+public class SnakeSegment : MonoBehaviour
+{
+    public bool isMoving { get; private set; }
+    public Node NewNode { get; set; }
+    public IEnumerator MoveToNewNode(float speed, Action<SnakeSegment> callback)
+    {
+        if (NewNode == null) { yield break; }
+        isMoving = true;
+        while (true)
+        {
+            var dist = Vector3.Distance(transform.position, NewNode.WorldPosition);
+            if (Mathf.Approximately(dist, 0f)) 
+            {
+                transform.position = NewNode.WorldPosition;
+                isMoving = false;
+                NewNode = null;
+                callback(this);
+               yield break;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, NewNode.WorldPosition, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+}
