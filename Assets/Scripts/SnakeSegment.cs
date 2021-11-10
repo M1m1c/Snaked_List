2622765e2e6d42d5cbe2d0e0d91b61714ee6d0d5
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-11-10 16:38:54 +0100

    Changed movement code in snake segment

--- Assets/Scripts/SnakeSegment.cs
+++ Assets/Scripts/SnakeSegment.cs
@@ -4,26 +4,58 @@ using System.Collections;
 using UnityEngine;
 public class SnakeSegment : MonoBehaviour
 {
-    public bool isMoving { get; private set; }
-    public Node NewNode { get; set; }
-    public IEnumerator MoveToNewNode(float speed, Action<SnakeSegment> callback)
+   
+
+    public bool isMoving { get; protected set; }
+    public Node targetNode { get; set; }
+
+    [SerializeField] protected float moveSpeed = 1f;
+    protected Node currentNode = null;
+
+    protected int targetIndex = 0;
+
+    public IEnumerator MoveToTarget()
     {
-        if (NewNode == null) { yield break; }
-        isMoving = true;
         while (true)
         {
-            var dist = Vector3.Distance(transform.position, NewNode.WorldPosition);
-            if (Mathf.Approximately(dist, 0f)) 
+
+            var dist = Vector3.Distance(transform.position, targetNode.WorldPosition);
+            if (Mathf.Approximately(dist, 0f))
             {
-                transform.position = NewNode.WorldPosition;
+                targetIndex++;
+                currentNode = targetNode;
+                targetNode = null;
                 isMoving = false;
-                NewNode = null;
-                callback(this);
-               yield break;
+                yield break;
             }
-            transform.position = Vector3.MoveTowards(transform.position, NewNode.WorldPosition, speed * Time.deltaTime);
+
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                targetNode.WorldPosition,
+                moveSpeed * Time.deltaTime);
+
             yield return null;
         }
     }
+
+    //public IEnumerator MoveToNewNode(float speed, Action<SnakeSegment> callback)
+    //{
+    //    if (NewNode == null) { yield break; }
+    //    isMoving = true;
+    //    while (true)
+    //    {
+    //        var dist = Vector3.Distance(transform.position, NewNode.WorldPosition);
+    //        if (Mathf.Approximately(dist, 0f)) 
+    //        {
+    //            transform.position = NewNode.WorldPosition;
+    //            isMoving = false;
+    //            NewNode = null;
+    //            callback(this);
+    //           yield break;
+    //        }
+    //        transform.position = Vector3.MoveTowards(transform.position, NewNode.WorldPosition, speed * Time.deltaTime);
+    //        yield return null;
+    //    }
+    //}
 
 }
