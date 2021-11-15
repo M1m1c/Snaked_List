ae50477c091347055c9c91c509b16d1e8b41e820
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-11-15 17:30:32 +0100

    Created fruit spawner and added it as a component to play area

---
+++ Assets/Scripts/FruitSpawner.cs
@@ -0,0 +1,43 @@
+using m1m1c_3DAstarPathfinding;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FruitSpawner : MonoBehaviour
+{
+    public static UnityEvent<Node> FruitSpawnEvent = new UnityEvent<Node>();
+
+    public FruitPickup FruitPrefab;
+
+    private NavVolume navVolume;
+
+    private void Start()
+    {
+        navVolume = GetComponent<NavVolume>();
+        StartCoroutine(LoadTimer());
+    }
+
+    private IEnumerator LoadTimer()
+    {
+        yield return new WaitForSeconds(0.5f);
+        SpawnFruit();
+    }
+
+    private void SpawnFruit()
+    {
+        Node spawnNode;
+        while (true)
+        {
+            spawnNode = navVolume.GetRandomNode();
+            if (spawnNode.Walkable)
+            {
+                break;
+            }
+        }
+
+        var tempRef = Instantiate(FruitPrefab, spawnNode.WorldPosition, Quaternion.identity);
+        tempRef.FruitEatenEvent.AddListener(SpawnFruit);
+        FruitSpawnEvent.Invoke(spawnNode);
+    }
+
+}
