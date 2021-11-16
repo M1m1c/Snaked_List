52e5bf4be27726ea8972b8a962f1babc152cc9e2
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-11-16 11:32:29 +0100

    added ability to spawn up to eight snakes, wiht predetermined spawn points

--- Assets/Scripts/SnakeSpawner.cs
+++ Assets/Scripts/SnakeSpawner.cs
@@ -3,15 +3,35 @@ using UnityEngine;
 
 public class SnakeSpawner : MonoBehaviour
 {
-    public Transform SnakeParent;
-    public SnakeHead SnakePrefab;
-    public Vector3Int SpawnCoordinate = new Vector3Int(1, 1, 1);
+    public GameObject SnakePrefab;
+
+    [Range(1,8)]public int SnakesToSpawn = 1;
+
+    private Vector3Int[] SpawnCoordinates = new Vector3Int[] 
+    { 
+        new Vector3Int(1, 1, 1),
+        new Vector3Int(18,18,18),
+        new Vector3Int(1, 18, 18),
+        new Vector3Int(18, 1, 18),
+        new Vector3Int(18,18,1),
+        new Vector3Int(18,1,1),
+        new Vector3Int(1,18,1),
+        new Vector3Int(1,1,18),
+    };
+
     void Start()
     {
         var navVolume = GetComponent<NavVolume>();
-        var node = navVolume.GetNodeFromIndex(SpawnCoordinate);
-        var pos = node.WorldPosition;
-        var snakeInstance = Instantiate(SnakePrefab, pos, Quaternion.identity,SnakeParent);
-        snakeInstance.Setup(node);
+
+        for (int i = 0; i < SnakesToSpawn; i++)
+        {
+            var node = navVolume.GetNodeFromIndex(SpawnCoordinates[i]);
+            if (node == null) { continue; }
+
+            var pos = node.WorldPosition;
+            var snakeInstance = Instantiate(SnakePrefab, pos, Quaternion.identity);
+            var snakeHead = snakeInstance.GetComponentInChildren<SnakeHead>();
+            snakeHead.Setup(node);
+        }
     }
 }
