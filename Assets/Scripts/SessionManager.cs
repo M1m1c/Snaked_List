66f566081822317e5f2f32136eaa667a1a34f941
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-11-17 23:36:57 +0100

    made session manager spawn and update score slot UI and hold score keepers

--- Assets/Scripts/SessionManager.cs
+++ Assets/Scripts/SessionManager.cs
@@ -11,16 +11,27 @@ public class SessionManager : MonoBehaviour
     public static UnityEvent EndSessionEvent = new UnityEvent();
     public static bool isInSession { get; private set; }
 
+    public Heap<SnakeScoreKeeper> snakeScores = new Heap<SnakeScoreKeeper>(8);
+
     public GameObject SetupUI;
 
     public Text SnakeSlideNumber;
     public Text PlayTimeSlideNumber;
     public Text PlayTimerText;
 
+    public RectTransform ScoreSlotHolder;
+    public SnakeScoreSlot scoreSlotPrefab;
+
     private SnakeSpawner snakeSpawner;
+
+    private List<SnakeScoreSlot> snakeScoreSlots = new List<SnakeScoreSlot>();
 
     private float playTime = 60f;
     private float sessionTimer = 0f;
+
+
+    private float originalScoreSlotYOffset = 110;
+    private float scoreSlotYOffset = 110;
 
     void Start()
     {
@@ -34,6 +45,24 @@ public class SessionManager : MonoBehaviour
         sessionTimer += Time.deltaTime;
         PlayTimerText.text = $"{sessionTimer.ToString("F2")} / {playTime}";
 
+        foreach (var score in snakeScores)
+        {
+            snakeScores.UpdateItem(score);
+        }
+
+        var index = 0;
+        foreach (var score in snakeScores)
+        {
+            if (index > snakeScoreSlots.Count) { break; }
+            var slot = snakeScoreSlots[index];
+            if (slot == null) { continue; }
+            slot.SetImageColor(score.SnakeColor);
+            slot.SetSnakeIndex(score.SnakeIndex);
+            slot.SetFruitsEaten(score.FruitsEaten);
+            slot.SetLifeTime(score.LifeTime);
+            index++;
+        }
+
         if (sessionTimer >= playTime)
         {
             isInSession = false;
@@ -45,9 +74,51 @@ public class SessionManager : MonoBehaviour
     public void StartSession()
     {
         if (SetupUI) { SetupUI.SetActive(false); }
+
+
+        snakeScores.Clear();
         StartSessionEvent.Invoke();
         sessionTimer = 0f;
         isInSession = true;
+
+
+        if (snakeScoreSlots.Count > 0)
+        {
+            for (int i = snakeScoreSlots.Count - 1; i >= 0; i--)
+            {
+                Destroy(snakeScoreSlots[i].gameObject);
+            }
+            snakeScoreSlots = new List<SnakeScoreSlot>();
+        }
+
+        scoreSlotYOffset = originalScoreSlotYOffset;
+        var firstTime = false;
+        foreach (var item in snakeScores)
+        {
+            if (item == null) { continue; }
+
+            var slot = Instantiate(
+                scoreSlotPrefab,
+                ScoreSlotHolder.position - new Vector3(0f, scoreSlotYOffset, 0f),
+                Quaternion.identity,
+                ScoreSlotHolder);
+
+            if (!firstTime)
+            {
+                firstTime = true;
+                var slotTransform = slot.gameObject.transform;
+                slotTransform.localScale += new Vector3(.3f, .3f, .3f);
+                slotTransform.position += new Vector3(-39f, 20f, 0f);
+            }
+
+            scoreSlotYOffset += originalScoreSlotYOffset;
+
+            slot.SetImageColor(item.SnakeColor);
+            slot.SetSnakeIndex(item.SnakeIndex);
+
+            snakeScoreSlots.Add(slot);
+
+        }
     }
 
     public void SetSnakesToSpawn(System.Single value)
